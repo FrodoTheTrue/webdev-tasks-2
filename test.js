6c97abc59d9af99788b7e92cd6a37e44b388f7d8
Fedor <mydearfrodo@yandex.ru>
2016-02-27 22:41:44 +0500

    feat():init

---
+++ test.js
@@ -0,0 +1,16 @@
+'use strict';
+var multivarka = require('./multivarka');
+multivarka
+    // Указываем url для подключения
+    .server('mongodb://localhost/urfu-2015')
+
+    // и коллекцию
+    .collection('students')
+
+    // Выбираем только те записи, в которых поле `group` равно значению «ПИ-301».
+    .where('group').not().lessThan(3)
+
+    // После подготовки, делаем запрос
+    .find(function (data) {
+        console.log(data);
+    });
